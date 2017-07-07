ac6b8b4adf8f81c714f2af3223a164cef671658b
FranzPoize <francois.poizat@gmail.com>
2017-07-07 16:47:38 +0200

    using transform runtime

--- external.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-var config = require('./base.config.js');
-
-config.entry.ptypo.unshift('babel-polyfill');
-config.output.path = path.join(__dirname, 'external_dist');
-
-module.exports = config;
